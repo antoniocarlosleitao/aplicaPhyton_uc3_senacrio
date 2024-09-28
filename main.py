c7ca9d0fe3360fa00d45157ef84be620617dda5c
Antonio Carlos da Silva Leitão <antoniocarlosdasilvaleitao4@gmail.com>
2024-09-27 21:16:46 -0300

    add: inclusão gi ignore no projeto

---
+++ main.py
@@ -0,0 +1,19 @@
+#Escreva um programa que verifique se um número é par ou ímpar.
+numero = int(input('Digite um número: '))
+if numero % 2 == 0
+    print('O número é par!')
+else:
+    print('O número é par')
+# Desenvolva um algoritmo que peça oa usuário sua idade e
+# retorne se ele pode votar ou não.
+
+
+
+idade = int(input("Digite sua idade: ")) #input
+if idade >= 16: #estrutura de decisão composta
+    print("Você pode votar!")
+else:
+    print("Você não pode votar!")
+
+
+
